cb7275a8fa4830a0823d538a65e9104ab2b6b18c
Diego Mullo <diego.mullo@epn.edu.ec>
2024-10-17 09:41:27 -0500

    Commit 2

--- src/jugador.java
+++ src/jugador.java
@@ -1,5 +1,38 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class jugador {
+    String nickname;
+    int edad;
+    String pais;
 
+    public jugador(){
+        nickname="Goddie";
+        edad=22;
+        pais="Ecuador";
+    }
+
+    public void patear(){
+        System.out.println("El jugador "+nickname+ " con "+edad+" años de edad del país "+pais+" → patea.");
+    }
+    public void correr(){
+        System.out.println("El jugador "+nickname+ " con "+edad+" años de edad del país "+pais+" → corre.");
+    }
+
+    public jugador(String nickname, int edad, String pais){
+        this.nickname=nickname;
+        this.edad = edad;
+        this.pais=pais;
+    }
+
+
+    public void patearDos(){
+        System.out.println("El jugador "+nickname+ " con "+edad+" años de edad del país "+pais+" → patea.");
+    }
+    public void correrDos(){
+        System.out.println("El jugador "+nickname+ " con "+edad+" años de edad del país "+pais+" → corre.");
+    }
+    public void saltar(int s){
+        int saltos = 2+s;
+        System.out.println("El jugador "+nickname+ " con "+edad+" años de edad del país "+pais+" → salta "+saltos+" veces.");
+    }
 }
--- src/llamarJugador.java
+++ src/llamarJugador.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class llamarJugador {
+    public static void main(String[] args) {
+        jugador player1 = new jugador();
+
+        player1.patear();
+        player1.correr();
+        System.out.println();
+
+        jugador player2 = new jugador("Macaquinho",21,"Venezuela");
+
+        player2.patearDos();
+        player2.correrDos();
+        player2.saltar(2);
+    }
 }
